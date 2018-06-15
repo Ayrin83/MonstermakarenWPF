3e61aecc162792bbe12ab9828296e094a8409dd9
camilla <camilla.westrin@faltcom.se>
2018-06-16 01:20:28 +0200

    Added WritableBitmap-version drawing of "stitches" (so far squares). Seems to work well :)

--- MonstermakarenWPF/MainWindow.xaml.cs
+++ MonstermakarenWPF/MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace MonstermakarenWPF
         {
             Logger.Log("buttonRectangular_Click");
             patternwindow.Show();
-            patternwindow.drawRectangular(100, 150);
+            patternwindow.drawRectangular(10, 5);
             //patternwindow.drawRectangular(Convert.ToInt32(textBoxNumTotalHorizontalSt), Convert.ToInt32(textBoxNumTotalVerticalSt));
         }
     }
--- MonstermakarenWPF/PatternWindow.xaml.cs
+++ MonstermakarenWPF/PatternWindow.xaml.cs
@@ -54,51 +54,129 @@ namespace MonstermakarenWPF
 
         //}
 
+        //public void drawRectangular(int numHorizontal, int numVertical)
+        //{
+        //    //List<Point> verticalPoints = new List<Point>(numHorizontal + 1);
+        //    const int MARGIN = 20;
+        //    double distanceX = (myCanvas.Width - 3*MARGIN) / numHorizontal;
+        //    double distanceY = (myCanvas.Height - 3*MARGIN) / numVertical;
+            
+        //    // Create a path to draw a geometry with.
+        //    Path myPath = new Path();
+        //    myPath.Stroke = Brushes.Black;
+        //    myPath.StrokeThickness = 1;
+
+        //    // Create a StreamGeometry to use to specify myPath.
+        //    StreamGeometry geometry = new StreamGeometry();
+        //    geometry.FillRule = FillRule.EvenOdd;
+
+        //    using (StreamGeometryContext sgc = geometry.Open())
+        //    {
+        //        for (int x = 0; x < numHorizontal; x++)
+        //        {
+        //            for (int y = 0; y < numVertical; y++)
+        //            {
+        //                sgc.BeginFigure(new Point(MARGIN + distanceX * x, MARGIN + distanceY * y), true, true);
+        //                sgc.LineTo(new Point(MARGIN + distanceX * (x + 1), MARGIN + distanceY * y), true, false);
+        //                sgc.LineTo(new Point(MARGIN + distanceX * (x + 1), MARGIN + distanceY * (y + 1)), true, false);
+        //                sgc.LineTo(new Point(MARGIN + distanceX * x, MARGIN + distanceY * (y + 1)), true, false);
+
+        //                //verticalPoints[x + y * x] = new Point(10 + distanceX * x, 10 + distanceY * );
+        //            }
+        //        }
+        //        //sgc.BeginFigure(new Point(10, 10), true, true);
+        //        //sgc.LineTo(new Point(10, 100), true, false);
+        //        //sgc.LineTo(new Point(100, 100), true, false);
+        //        //sgc.LineTo(new Point(100, 10), true, false);
+        //        sgc.Close();
+        //    }
+        //    myPath.Data = geometry;
+        //    myCanvas.Children.Add(myPath);
+            
+        //}
+
         public void drawRectangular(int numHorizontal, int numVertical)
         {
-            //List<Point> verticalPoints = new List<Point>(numHorizontal + 1);
+            WriteableBitmap writeablebitmap = new WriteableBitmap((int)this.ActualHeight, (int)this.ActualWidth, 96, 96, PixelFormats.Bgr32, null);
+
+            myImage.Source = writeablebitmap;
+
+            myImage.Stretch = Stretch.None;
+            myImage.HorizontalAlignment = HorizontalAlignment.Left;
+            myImage.VerticalAlignment = VerticalAlignment.Top;
+
             const int MARGIN = 20;
-            double distanceX = (myCanvas.Width - 3*MARGIN) / numHorizontal;
-            double distanceY = (myCanvas.Height - 3*MARGIN) / numVertical;
-            
-            // Create a path to draw a geometry with.
-            Path myPath = new Path();
-            myPath.Stroke = Brushes.Black;
-            myPath.StrokeThickness = 1;
+            int numPixelsInRow = (int)myImage.Width - 3 * MARGIN;
+            double distanceX = (myImage.Width - 3 * MARGIN) / numHorizontal;
+            double distanceY = (myImage.Height - 3 * MARGIN) / numVertical;
 
-            // Create a StreamGeometry to use to specify myPath.
-            StreamGeometry geometry = new StreamGeometry();
-            geometry.FillRule = FillRule.EvenOdd;
+            int distX_int = (int)Math.Floor(distanceX);
+            int distY_int = (int)Math.Floor(distanceY);
 
-            using (StreamGeometryContext sgc = geometry.Open())
+            Int32 white = 0x00ffffff; // RGB
+            Int32 black = 0x00000000; // RGB
+
+            int pBackbuffer = (int)writeablebitmap.BackBuffer;
+            //pBackbuffer += MARGIN * writeablebitmap.BackBufferStride; // jump MARGIN number of rows
+
+            int startOfRow;
+ 
+            //pBackbuffer += MARGIN * 4; // Jump MARGIN number of columns 
+
+            writeablebitmap.Lock();
+
+            unsafe
             {
-                for (int x = 0; x < numHorizontal; x++)
+                for (int y = 0; y < myImage.Height; y++)
                 {
-                    for (int y = 0; y < numVertical; y++)
-                    {
-                        sgc.BeginFigure(new Point(MARGIN + distanceX * x, MARGIN + distanceY * y), true, true);
-                        sgc.LineTo(new Point(MARGIN + distanceX * (x + 1), MARGIN + distanceY * y), true, false);
-                        sgc.LineTo(new Point(MARGIN + distanceX * (x + 1), MARGIN + distanceY * (y + 1)), true, false);
-                        sgc.LineTo(new Point(MARGIN + distanceX * x, MARGIN + distanceY * (y + 1)), true, false);
+                    startOfRow = pBackbuffer;
 
-                        //verticalPoints[x + y * x] = new Point(10 + distanceX * x, 10 + distanceY * );
+                    if (y == MARGIN || y > MARGIN && (y - MARGIN) % distY_int == 0) // Horisontal line
+                    {
+                        for (int x = 0; x < myImage.Width; x++)
+                        {
+                            if (x > MARGIN && x - MARGIN < distX_int*numHorizontal)
+                            {
+                                *((int*)pBackbuffer) = black;
+                            }
+                            else
+                            {
+                                *((int*)pBackbuffer) = white;
+                            }
+                            pBackbuffer += 4;
+                        }
                     }
-                }
-                //sgc.BeginFigure(new Point(10, 10), true, true);
-                //sgc.LineTo(new Point(10, 100), true, false);
-                //sgc.LineTo(new Point(100, 100), true, false);
-                //sgc.LineTo(new Point(100, 10), true, false);
-                sgc.Close();
+                    else
+                    {
+                        for (int x = 0; x < myImage.Width; x++)
+                        {
+                            if (y >= MARGIN && y - MARGIN <= distY_int * numVertical && x >= MARGIN && x - MARGIN <= distX_int * numHorizontal && (x - MARGIN) % distX_int == 0)
+                            {
+                                *((int*)pBackbuffer) = black;
+                            }
+                            else
+                            {
+                                *((int*)pBackbuffer) = white;
+                            }
+                            pBackbuffer += 4;
+                        }
+                    }                 
+
+                    //backMargin = writeablebitmap.BackBufferStride - (pBackbuffer - startOfRow);
+                    //pBackbuffer += backMargin;
+                   
+                }                               
             }
-            myPath.Data = geometry;
-            myCanvas.Children.Add(myPath);
-            
+
+            writeablebitmap.AddDirtyRect(new Int32Rect(0, 0, (int)this.ActualWidth, (int)this.ActualHeight));
+            writeablebitmap.Unlock();
         }
 
-        private void myCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+
+        private void myImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
            
-            Point hit = e.GetPosition(myCanvas);
+            Point hit = e.GetPosition(myImage);
 
             Logger.Log("Hit! " + hit.ToString());
         }
